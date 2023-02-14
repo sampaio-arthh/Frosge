f4bf54f083c7e02922767fe7a0967fd433b43fe3
Artheir <sampaio.thpp@gmail.com>
2023-02-14 14:03:39 -0300

    1st stage develop

---
+++ app.js
@@ -0,0 +1,6 @@
+hamburguer = document.querySelector(".hamburguer-menu");
+hamburguer.onclick = function() {
+    navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("active");
+    
+}
